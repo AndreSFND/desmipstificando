6a75d3915f727a20f862a9ea31792e7ee72b7867
l-a-motta <l.a.motta@usp.br>
2020-07-21 17:29:07 -0300

    Constructed backend classes
    Still work in progress, will be defined more later

--- src/_classes/Questao.ts
+++ src/_classes/Questao.ts
@@ -1,9 +1,20 @@
-class Questao {
+// Temos uma classe Questao abstrata, pois ela eh so a base para os varios tipos de questoes
+// No nosso programa
+abstract class Questao {
 
-    constructor() {
+    //Todas as questoes tem um enunciado, obrigatoriamente
+    private enunciado: string;
 
-        
+    public getEnunciado(): string {
+        return this.enunciado;
+    }
 
+    public setEnunciado(enunciado: string): void {
+        this.enunciado = enunciado;
+    }
+
+    constructor(enunciado: string) {
+        this.setEnunciado(enunciado);
     }
 
 }
--- src/_classes/Questoes/Alternativa.ts
+++ src/_classes/Questoes/Alternativa.ts
@@ -1,9 +1,47 @@
 class Alternativa extends Questao {
+    private alternativas: string[];
+    private correta: number;
+    private acertou: boolean;
 
-    constructor() {
+    public getAlternativas(): string[] {
+        return this.alternativas;
+    }
 
-        super();
+    public setAlternativas(alternativas: string[]): void {
+        this.alternativas = alternativas;
+    }
 
+    public getCorreta(): number {
+        return this.correta;
+    }
+
+    public setCorreta(correta: number): void {
+        this.correta = correta;
+    }
+
+    public isAcertou(): boolean {
+        return this.acertou;
+    }
+
+    public setAcertou(acertou: boolean): void {
+        this.acertou = acertou;
+    }
+
+    constructor(enunciado: string, alternativas: string[], correta: number) {
+        super(enunciado);
+        this.setAlternativas(alternativas);
+        this.setCorreta(correta);
+    }
+
+    public checa(sugerido: number) {
+        if(sugerido == this.correta)
+        {
+            this.setAcertou(true);
+        }
+        else
+        {
+            this.setAcertou(false);
+        }
     }
 
 }
