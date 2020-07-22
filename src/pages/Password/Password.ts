8e9b4bed8b4f40c0b5efbbbc8d36a17d33db0fd5
Andre Santana <andre_sfnd@usp.br>
2020-07-21 22:13:54 -0300

    fix

--- src/pages/Password/Password.ts
+++ src/pages/Password/Password.ts
@@ -14,8 +14,13 @@ class Password extends Tela {
     OnEnter() {
 
         // @ts-ignore
-        $("#root").load("./src/pages/Password", () => {
+        var el = $('<div></div>').load("./src/pages/Password", () => {
 
+            // @ts-ignore
+            $("#root").append(el);
+            
+            Main.moveRight();
+            
             /**
              * Define a interacao do PIN (avancar apos inserir um valor)
              */
